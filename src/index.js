8e0272bbc188a72b3c596eb8ae1070cb8ddeb762
Leo <464362353@qq.com>
2018-01-08 00:31:18 +0800

    Redux demo

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
+  }
+}
+function stateChanger (state, action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text : action.text
+        }
+      }
+      break
+    case 'UPDATE_TITLE_COLOR':
+      return {
+        ...state,
+        color: {
+          ...state.title,
+          color: action.color
+        }
+      }
+      break
+    default:
+      return state
+  }
+}
+function createStore (state, stateChanger) {
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = stateChanger(state, action) // 覆盖原对象
+    listeners.forEach((listener) => listener())
+  }
+  return { getState, dispatch, subscribe }
+}
+function renderApp (newAppState, oldAppState = {}) {
+  if(newAppState === oldAppState) return
+  console.log('render app...')
+  renderTitle(newAppState.title,oldAppState.title)
+  renderContent(newAppState.content, oldAppState.content)
+}
+function renderTitle (newTitle, oldTitle = {}) {
+  if(newTitle === oldTitle) return
+  console.log('render title...')
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = newTitle.text
+  titleDOM.style.color = newTitle.color
+}
+
+function renderContent (newContent, oldContent) {
+  if(newContent === oldContent) return 
+  console.log('render content...')
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = newContent.text
+  contentDOM.style.color = newContent.color
+}
+
+const store = createStore(appState, stateChanger)
+let oldState = store.getState()
+
+store.subscribe(() => {
+  const newState = store.getState()
+  renderApp(newState, oldState)
+  oldState = newState
+})
+
+renderApp(store.getState()) // 首次渲染页面
+store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
+store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
