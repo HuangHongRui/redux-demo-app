1c628e369de666f8e90ebecdc086aeb5b3e06aee
Leo <464362353@qq.com>
2018-01-14 01:09:08 +0800

    结合 context 和 store

--- src/ThemeSwitch.js
+++ src/ThemeSwitch.js
@@ -1,11 +1,40 @@
-import React,{ Component, propTypes } from 'react'
+import React,{ Component } from 'react'
+import PropTypes from 'prop-types'
 
 class ThemeSwitch extends Component{
+  static contextTypes = {
+    store : PropTypes.object
+  }
+  constructor() {
+    super()
+    this.state = {
+      themeColor : ''
+    }
+  }
+  componentWillMount() {
+    let { store } = this.context
+    this._updateThemeColor()
+    store.subscribe(() => this._updateThemeColor())
+  }
+  _updateThemeColor() {
+    let { store } = this.context
+    const state = store.getState()
+    this.setState({
+      themeColor: state.themeColor
+    })
+  }
+  handleSwitchColor(color) {
+    let { store } = this.context
+    store.dispatch({
+      type: 'CHANGE_COLOR',
+      themeColor: color
+    }) 
+  }
   render(){
     return (
       <div>
-        <button>YELLOW</button>
-        <button>RED</button>
+        <button onClick={ this.handleSwitchColor.bind(this,'yellow')} style={{ color : this.state.themeColor}}>YELLOW</button>
+        <button onClick={ this.handleSwitchColor.bind(this,'blue')} style={{ color : this.state.themeColor}}>RED</button>
       </div>
     )
   }
